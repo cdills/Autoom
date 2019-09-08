2f97ab1e8708c0949b78686d3a6f7fa9fe974d2a
cody.dills <cody.dills@globant.com>
2019-09-08 17:26:18 -0400

    add event frame and event listener

--- Autoom.lua
+++ Autoom.lua
@@ -1,10 +1,16 @@
-# Contants
-local playerCurrentMana = UnitMana("player");
-local playerMaxMana = UnitMaxMana("player"); 
+local manaEventFrame = CreateFrame("Frame"); 
+manaEventFrame:RegisterEvent("COMBAT_TEXT_UPDATE"); 
+manaEventFrame:SetScript("OnEvent", checkMana(event)); 
 
-if (playerCurrentMana < playerMaxMana * 0.1)
- then sendChatWarning();
-end
+
+function checkMana(event) 
+    local playerCurrentMana = UnitMana("player");
+    local playerMaxMana = UnitMaxMana("player"); 
+    
+    if (playerCurrentMana < playerMaxMana * 0.1)
+        then sendChatWarning();
+    end
+end 
 
 function sendChatWarning ()
     SendChatMessage("I am out of mana", "PARTY");
