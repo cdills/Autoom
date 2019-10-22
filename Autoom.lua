c3de358405e5d3ed9639aadfa9281fc143a82c36
cody.dills <cody.dills@globant.com>
2019-10-22 12:22:41 -0400

    use correct power API

--- Autoom.lua
+++ Autoom.lua
@@ -1,17 +1,41 @@
-local manaEventFrame = CreateFrame("Frame"); 
-manaEventFrame:RegisterEvent("COMBAT_TEXT_UPDATE"); 
-manaEventFrame:SetScript("OnEvent", checkMana(event)); 
+autoomOptions = {};
+autoomOptions.powerThreshold = .1;
+
+local autoomEventFrame = CreateFrame("Frame");
+autoomEventFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
+autoomEventFrame:RegisterEvent("UNIT_MODEL_CHANGED");
+autoomEventFrame:RegisterEvent("UNIT_LEVEL");
+autoomEventFrame:RegisterEvent("UNIT_RESISTANCES");
+autoomEventFrame:RegisterEvent("UNIT_STATS");
+autoomEventFrame:RegisterEvent("UNIT_DAMAGE");
+autoomEventFrame:RegisterEvent("UNIT_RANGEDDAMAGE");
+autoomEventFrame:RegisterEvent("PLAYER_DAMAGE_DONE_MODS");
+autoomEventFrame:RegisterEvent("UNIT_ATTACK_SPEED");
+autoomEventFrame:RegisterEvent("UNIT_ATTACK_POWER");
+autoomEventFrame:RegisterEvent("UNIT_RANGED_ATTACK_POWER");
+autoomEventFrame:RegisterEvent("UNIT_ATTACK");
+autoomEventFrame:RegisterEvent("PLAYER_GUILD_UPDATE");
+autoomEventFrame:RegisterEvent("SKILL_LINES_CHANGED");
+autoomEventFrame:RegisterEvent("COMBAT_LOG_EVENT");
 
 
-function checkMana(event) 
-    local playerCurrentMana = UnitMana("player");
-    local playerMaxMana = UnitMaxMana("player"); 
+autoomEventFrame:SetScript("OnEvent",
+    function(self, event, ...)
+        autoomEventFrame.CheckMana();
+    end)
+
+
+function autoomEventFrame:CheckMana() 
     
-    if (playerCurrentMana < playerMaxMana * 0.1)
-        then sendChatWarning();
+    local playerMaxMana = UnitPowerMax("player", 0);
+    local playerCurrentMana = UnitPower("player", 0);
+    
+    if (playerCurrentMana < playerMaxMana * autoomOptions.powerThreshold)
+        then autoomEventFrame:sendChatWarning(playerCurrentMana);
     end
 end 
 
-function sendChatWarning ()
-    SendChatMessage("I am out of mana", "PARTY");
+
+function autoomEventFrame:sendChatWarning (remainingMana)
+    SendChatMessage("I have" .. tostring(remainingMana) .. " mana remaining", "party", nil, nil);
 end
